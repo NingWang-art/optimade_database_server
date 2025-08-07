e190f961aa2c5c0d8f206eed7a5723edb4693280
NingWang <84500213+NingWang-art@users.noreply.github.com>
2025-08-07 15:56:25 +0800

    remove oss upload part from server

--- Optimade_Server/bohrium_setup_env.sh
+++ Optimade_Server/bohrium_setup_env.sh
@@ -10,12 +10,6 @@ rm -f .curlrc .wgetrc
 export http_proxy='http://ga.dp.tech:8118'
 export https_proxy='http://ga.dp.tech:8118'
 
-export BOHRIUM_BOHRIUM_URL="https://bohrium.test.dp.tech"
-export BOHRIUM_TIEFBLUE_URL="https://tiefblue.test.dp.tech"
-export BOHRIUM_OPENAPI_URL="https://openapi.test.dp.tech"
-export BOHRIUM_BASE_URL="https://openapi.test.dp.tech"
-export TIEFBLUE_BASE_URL="https://tiefblue-zjk-test.test.dp.tech"
-
 # Activate conda environment
 source /root/.bashrc  # Adjust path if needed
 conda activate optimade
